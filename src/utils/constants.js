50e7f89e81713f834df0c7aa51f6643dd4a3c1aa
geromedina <gerocode768@gmail.com>
2024-03-01 09:03:48 -0300

    cambio variables

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const ENV = {
-  SERVER_HOST: "http://127.0.0.1:1337",
-  API_URL: "http://127.0.0.1:1337/api",
+  SERVER_HOST: "https://servidor-gaming-strapi.up.railway.app",
+  API_URL: "https://servidor-gaming-strapi.up.railway.app/api",
   ENDPOINTS: {
     AUTH: {
       REGISTER: "auth/local/register",
